2e7d7bc763739e725395afb13fbfb3a92f08ae73
SHRIYASH SHARMA <shriyashsharma01@gmail.com>
2018-03-19 07:08:29 +0530

    Initial Commit

---
+++ Mvc/Data/MvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mvc.Models
+{
+    public class MvcContext : DbContext
+    {
+        public MvcContext (DbContextOptions<MvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Mvc.Models.Movie> Movie { get; set; }
+    }
+}
--- Mvc/Startup.cs
+++ Mvc/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Mvc.Models;
 
 namespace Mvc
 {
@@ -22,6 +24,9 @@ namespace Mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<MvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
